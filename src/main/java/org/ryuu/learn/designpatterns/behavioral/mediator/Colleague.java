a9a0304ae59c67cd30dbd2ee0876b1dd04b0cd29
Ryuu <2357622935@qq.com>
2025-09-22 16:02:20 +0800

    refactor: mediator

--- src/main/java/org/ryuu/learn/designpatterns/behavioral/mediator/Colleague.java
+++ src/main/java/org/ryuu/learn/designpatterns/behavioral/mediator/Colleague.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 每个同事（{@link Colleague}）都知道他的中介者（{@link Mediator}）
+ * 每个同事在需要与其他同事通信时，与他的中介者通信
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface Colleague {
--- src/main/java/org/ryuu/learn/designpatterns/behavioral/mediator/Mediator.java
+++ src/main/java/org/ryuu/learn/designpatterns/behavioral/mediator/Mediator.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 中介者（{@link Mediator}）定义一个接口用于和各同事（{@link Colleague}）通信
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface Mediator {
